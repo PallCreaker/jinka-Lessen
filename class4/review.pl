96417a0eff61551dbfc6a2f023ff939c21e57b38
kazumasa Fujita <pallcreaker@gmail.com>
2013-10-17 17:27:17 +0900

    add review row and col

---
+++ class4/review.pl
@@ -0,0 +1,23 @@
+open (FH, "< ./test.dat") or die;	# データファイルを開く
+
+for ($i=0;$i < 9; $i++ ){
+	$line = <FH>;
+	chomp $line;
+	@data = split (/,/,$line);
+		foreach(@data){
+        $total += $_;
+    }
+    $data_count = @data;
+    for (my $p = 0; $p < $data_count; $p++) {
+    	${"line_".$p} += $data[$p];
+    }
+
+    #要素の置換
+	$line =~ s/,/\t/g; #/gですべて変換
+	print $line." ".$sum."=>".$total,"\n";
+}
+for (my $j = 0; $j < 8; $j++) {
+	print "下＝".${'line_'.$j}."	";
+}
+print "\n";
+close(FH);
