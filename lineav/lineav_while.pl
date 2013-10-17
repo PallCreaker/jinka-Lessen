594d9cc538324fad47c821927e29e6f2f51b4e39
kazumasa Fujita <pallcreaker@gmail.com>
2013-10-17 19:47:10 +0900

    comp lineav_while.pl

--- lineav/lineav_while.pl
+++ lineav/lineav_while.pl
@@ -1,24 +1,21 @@
 #!/usr/bin/perl
-
 # CSV形式のデータファイルを使用して行の平均を出力するスクリプト
-
 
 open (IN, "< test_data9-10.csv") or die;
 open (OUT,"> test_while_ave.out") or die;   # 書き出し用のファイルハンドラを設定
 
-while ($line=<IN>) {  # $lineに一行ずつ入れつつ<IN>が存在するうちは繰り返す
+while ($line = <IN>) {  # $lineに一行ずつ入れつつ<IN>が存在するうちは繰り返す
     chomp $line;
     @data = split(/,/,$line);
-
-    for (				) {
-	
-	
-	    }
-
+    $count = @data;
+    $sum = 0;
+    for (my $i = 0; $i < $count; $i++) {
+    	$sum += $data[$i];
+    }
+    $p++;
+    print $p.":".$sum / $count,"\n";
     print OUT ;   # OUTに書き出す
-
-
 }
- 
+
 close(IN);
 close(OUT);
