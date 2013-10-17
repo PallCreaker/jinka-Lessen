d49aab3076c3c214a2e520ff4cc0e3f5d1048bee
kazumasa Fujita <pallcreaker@gmail.com>
2013-10-17 19:19:07 +0900

    maybe comp lineav_for

--- lineav/lineav_for.pl
+++ lineav/lineav_for.pl
@@ -5,16 +5,19 @@
 open (IN,"< test_data9-10.csv") or die;
 open (OUT,"> test_for_ave.out") or die;   # 書き出し用のファイルハンドラを設定
 
-for (				) {
-    $line= <IN>;
-    chomp $line;
-    @data = split(/,/,$line);
-
-    for (				) {
-   
-    }
-   
- }
- 
+# これは、微妙ですね。ダサい。
+for ($i=0;$i < 100;$i++) {
+	$line= <IN>;
+	if ($line != "" ) {
+	    chomp $line;
+	    @data = split(/,/,$line);
+	    $y_count = @data;
+	    $sum = 0;
+	    for ($x=0; $x < $y_count; $x++) {
+	    	$sum += $data[$x];
+	    }
+	    print $sum / $y_count,"\n";
+	 }
+}
  close(IN);   # ファイルを閉じる
  close(OUT);
